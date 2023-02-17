47ea7db163f50a2ce9968d6b1d88391e930a10b5
hansung1908 <hansung1908@naver.com>
2023-02-17 23:48:24 +0900

    log 적용 (aop 이용)

--- src/main/java/com/cos/aop/config/BindingAdvice.java
+++ src/main/java/com/cos/aop/config/BindingAdvice.java
@@ -1,15 +1,20 @@
 package com.cos.aop.config;
 
 import com.cos.aop.dto.CommonDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,8 +24,14 @@ import java.util.Map;
 @Aspect
 public class BindingAdvice {
 
+    private static final Logger log = LoggerFactory.getLogger(BindingAdvice.class);
+
     @Before("execution(* com.cos.aop.controller..*Controller.*(..))")
     public void BeforeTestCheck() {
+
+        // request 값 처리
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        System.out.println("주소: " + request.getRequestURI());
 
         System.out.println("전처리 로그");
     }
@@ -51,6 +62,10 @@ public class BindingAdvice {
 
                     for(FieldError error: bindingResult.getFieldErrors()) {
                         errorMap.put(error.getField(), error.getDefaultMessage());
+                        // 로그 레벨: error < warn < info < debug (레벨이 클수록 하위 로그를 출력, 예: info 레벨이면 error, warn, info만 출력)
+                        log.warn(type + "." + method + "() -> 필드: " + error.getField() + ", 메세지: " + error.getDefaultMessage());
+                        // 로그를 파일로 생성하면 관리가 힘들어 진다.
+                        log.debug(type + "." + method + "() -> 필드: " + error.getField() + ", 메세지: " + error.getDefaultMessage());
                     }
 
                     return new CommonDto<>(HttpStatus.BAD_REQUEST.value(), errorMap);
